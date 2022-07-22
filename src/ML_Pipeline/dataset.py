287367043eed64a5d95607ca3378c43fe88b34bd
Abhimanyu Dasarwar <63962258+Abhimanyu9539@users.noreply.github.com>
2022-07-22 17:10:34 +0530

    Update dataset.py

--- src/ML_Pipeline/dataset.py
+++ src/ML_Pipeline/dataset.py
@@ -2,8 +2,8 @@ import utils
 
 # function to read the data
 def read_data():
-    train=utils.convert_json_to_spacy("D:\\ProjectProd\\Resume_Parser\\input\\train_1.json") # train file
+    train=utils.convert_json_to_spacy("") # train file
 
-    test=utils.convert_json_to_spacy("D:\\ProjectProd\\Resume_Parser\\input\\test.json")     # test file
+    test=utils.convert_json_to_spacy("")     # test file
 
     return train,test
